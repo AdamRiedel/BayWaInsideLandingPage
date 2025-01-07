5f6af20f5f1e9886d5ead435fe0e61237f42dec7
AdamRiedel <Adam.Riedel@baywa.de>
2025-01-07 14:48:04 +0100

    Fix a "bug" in the index.html file and undo the java-script file just for one button

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const buttons = document.querySelectorAll(".download-button, .download-button-top");
+const btn = document.querySelector(".download-button");
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", function() {
-    let link = document.createElement("a");
-    link.setAttribute("type", "hidden");
-    link.href = "assets/baywaInside.apk";
-    link.download = "BayWa-Inside.apk";
-    document.body.appendChild(link);
-    link.click();
-    link.remove();
-  });
+btn.addEventListener("click", function() {
+  let link = document.createElement("a");
+  link.setAttribute("type", "hidden"),
+  link.href = "assets/baywaInside.apk",
+  link.download = "BayWa-Inside.apk",
+  document.body.appendChild(link),
+  link.click(),
+  link.remove()
 });
